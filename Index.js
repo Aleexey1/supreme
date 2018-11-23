32b2ef48f39402b10b5cc0ac402282552f083483
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-23 15:40:30 -0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -39,27 +39,6 @@ bot.on('ready', () =>{
         setInterval(() => setStatus(), 10000);  //10000 = 10Ms = 10 segundos
 });
 
-bot.on("message", async message => {
-    let coinAmt = Math.floor(Math.random() * 15) + 1;
-    let baseAmt = Math.floor(Math.random() * 15) + 1;
-    console.log(`${coinAmt} ; ${baseAmt}`);
-  
-    if(coinAmt === baseAmt){
-      coins[message.author.id] = {
-        coins: coins[message.author.id].coins + coinAmt
-      };
-    fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-      if (err) console.log(err)
-    });
-    let coinEmbed = new Discord.RichEmbed()
-    .setAuthor(message.author.username)
-    .setColor("#0000FF")
-    .addField("💸", `${coinAmt} coins added!`);
-  
-    message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
-  }
-});
-
 bot.on('message', message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
